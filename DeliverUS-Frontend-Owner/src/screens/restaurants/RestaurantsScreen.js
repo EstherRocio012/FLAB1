02ebdac5a9a9d12366af003a9a31f9829830c7cb
Esther Rocío <estherrociovalladolid@gmail.com>
2024-06-26 08:49:15 +0200

    DONE

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -4,8 +4,60 @@ import { StyleSheet, View, FlatList, Pressable } from 'react-native'
 import TextRegular from '../../components/TextRegular'
 import { getAll } from '../../api/RestaurantEndpoints'
 import * as GlobalStyles from '../../styles/GlobalStyles'
+import ImageCard from '../../components/ImageCard'
+import TextSemiBold from '../../components/TextSemibold'
+export default function RestaurantsScreen ({ navigation }) {
+  const [restaurants, setRestaurants] = useState([])
 
-export default function RestaurantsScreen({ navigation }) {
+  useEffect(() => {
+    console.log('Loading restaurants, please wait 2 seconds')
+    setTimeout(() => {
+      setRestaurants(getAll) // tenemos que importar la función getAll
+      console.log('Restaurants loaded')
+    }, 2000)
+  }, [])
+
+  const renderRestaurant = ({ item }) => {
+    return (
+      <Pressable style = { styles.row}
+      onPress= { () => {
+        navigation.navigate('RestaurantDetailsScreen', { id: item.id })
+      }}>
+        <TextRegular>
+          {item.name}
+        </TextRegular>
+      </Pressable>
+    )
+  }
+
+  const renderRestaurantWithImageCard = ({ item }) => {
+    return (
+      <ImageCard
+        imageUri={item.logo ? { uri: process.env.API_BASE_URL + '/' + item.logo } : undefined}
+        title={item.name}
+        onPress={() => {
+          navigation.navigate('RestaurantDetailScreen', { id: item.id })
+        }}
+      >
+          <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+          {item.averageServiceMinutes !== null &&
+            <TextSemiBold>Avg. service time: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.averageServiceMinutes} min.</TextSemiBold></TextSemiBold>
+          }
+          <TextSemiBold>Shipping: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.shippingCosts.toFixed(2)}€</TextSemiBold></TextSemiBold>
+      </ImageCard>
+    )
+  }
+
+  return (
+    <FlatList
+      style= { styles.container}
+      data={restaurants}
+      renderItem={renderRestaurant}
+      keyExtractor={item => item.id.toString()}
+      />
+  )
+}
+/*
   return (
     <View style={styles.container}>
       <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>Random Restaurant</TextRegular>
@@ -28,7 +80,7 @@ export default function RestaurantsScreen({ navigation }) {
       </Pressable>
     </View>
   )
-}
+} */
 
 const styles = StyleSheet.create({
   container: {
